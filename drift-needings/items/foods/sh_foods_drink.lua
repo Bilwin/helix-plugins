869b0d025c43d41ff276f4740330794927d42096
Bilwin <bilwin@minerva.pw>
2020-12-24 18:10:46 +0900

    Sounds changing

--- drift-needings/items/foods/sh_foods_drink.lua
+++ drift-needings/items/foods/sh_foods_drink.lua
@@ -4,6 +4,6 @@ ITEM.model = Model("models/props_junk/popcan01a.mdl")
 ITEM.description = "Packaged by a food distribution center for civilians, it is not known what is in it"
 ITEM.width = 1
 ITEM.height = 1
-ITEM.useSound = "litenetwork/food/drink.wav"
+ITEM.useSound = "npc/barnacle/barnacle_gulp1.wav"
 ITEM.RestoreSaturation = 35
 ITEM.RestoreSatiety = 0
