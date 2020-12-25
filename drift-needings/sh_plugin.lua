1ee29b7ae82e15f12e0f14c1579ec15f39f75d3e
Maxim <marf.zpd@gmail.com>
2020-12-25 15:41:33 +0900

    Update sh_plugin.lua

--- drift-needings/sh_plugin.lua
+++ drift-needings/sh_plugin.lua
@@ -3,6 +3,11 @@ PLUGIN.name = "Hunger & Thirst"
 PLUGIN.author = "Bilwin"
 PLUGIN.description = "..."
 PLUGIN.schema = "Any"
+PLUGIN.license = [[
+    Copyright 2020 Maxim Sukharev (Bilwin)
+    This plugin is protected under by MPL-2.0 license
+    Full copy of license is here: https://www.mozilla.org/en-US/MPL/2.0/
+]]
 
 local ix = ix or {}
 ix.Hunger = ix.Hunger or {}
