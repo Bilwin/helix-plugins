bde84bbb4b51fd3c2649d059f36e9c7470deaf0c
Bilwin <bilwin@minerva.pw>
2020-12-25 15:26:41 +0900

    Clearitems update

--- sh_clearitems.lua
+++ sh_clearitems.lua
@@ -3,20 +3,31 @@ PLUGIN.author = "Bilwin"
 PLUGIN.description = "Clean your map from garbage"
 PLUGIN.schema = "Any"
 
-if (SERVER) then
+if ( SERVER ) then
+    util.AddNetworkString("ixClearItems::Notify")
+
     local ix = ix or {}
     ix.ClearItems = ix.ClearItems or {
-        ["ix_item"] = true,
-        ["ix_money"] = true,
-        ["ix_shipment"] = true
+        [ "ix_item" ]       = true,
+        [ "ix_money" ]      = true,
+        [ "ix_shipment" ]   = true
     }
+
+    local function IsEmptyTable( t )
+        return next( t ) == nil or false
+    end
 
     local function clear()
         timer.Create("ixCleanMap", 60 * 10, 0, function()
-            for _, v in pairs( ents.FindByClass("ix_*") ) do
+            for _, v in pairs( ents.FindByClass( "*" ) ) do
                 if ix.ClearItems[ v:GetClass() ] then
                     v:Remove()
                 end
+            end
+
+            if !IsEmptyTable(ix.ClearItems) then
+                net.Start("ixClearItems::Notify")
+                net.Broadcast()
             end
         end)
     end
@@ -24,4 +35,10 @@ if (SERVER) then
     function PLUGIN:InitPostEntity()
         clear()
     end
+elseif (CLIENT) then
+    net.Receive("ixClearItems::Notify", function()
+        if ( LocalPlayer() ) then
+            notification.AddLegacy("Мусор карты был очищен!")
+        end
+    end)
 end
