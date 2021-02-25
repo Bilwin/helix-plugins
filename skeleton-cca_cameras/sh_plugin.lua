0092d60702fd413890fbdf4c1eff90e5a753a755
Bilwin <bilwin@minerva.pw>
2021-02-25 19:42:05 +0900

    Update cameras skeleton

--- skeleton-cca_cameras/sh_plugin.lua
+++ skeleton-cca_cameras/sh_plugin.lua
@@ -1,12 +1,5 @@
-local plugin = PLUGIN
-plugin.name = "CCA Cameras"
-plugin.author = "You're here"
-plugin.description = "..."
-plugin.schema = "HL2 RP"
-plugin.readme = [[
-    Example plugin for camera NPC usage
 
-    if player has weapon_slam or etc. camera be triggered, and you can put some function for trigger (sv_hooks.lua 33 line)
-]]
+PLUGIN.name = "CCA Cameras"
+PLUGIN.schema = "HL2 RP"
 
-ix.util.Include("sv_hooks.lua")
+ix.util.Include("sv_hooks.lua", "server")
--- skeleton-cca_cameras/sv_hooks.lua
+++ skeleton-cca_cameras/sv_hooks.lua
@@ -1,57 +1,18 @@
 
-local distance, classname = 335, "npc_combine_camera"
-local _blacklist_weapon = {
-    ["weapon_smg1"] = true,
-    ["weapon_shotgun"] = true,
-    ["weapon_slam"] = true,
-    ["weapon_rpg"] = true,
-    ["weapon_ar2"] = true,
-    ["weapon_stunstick"] = true,
-    ["weapon_frag"] = true,
-    ["weapon_crowbar"] = true,
-    ["weapon_crossbow"] = true,
-    ["weapon_pistol"] = true
-}
-
--- idk why i do this
-function PLUGIN:EmitQueuedSound(ent, t_sound)
-    return ix.util.EmitQueuedSounds(ent, t_sound, 0, 0.1, 75, 100)
-end
-
 function PLUGIN:Think()
-    local _allents = ents.FindByClass(classname)
-    for i = 1, #_allents do
-        local ent = _allents[i]
-        local _buffer = ents.FindInSphere( ent:GetPos(), distance )
+    local cameras_ents = ents.FindByClass("npc_combine_camera")
 
-        for i = 1, #_buffer do
-            local pl = _buffer[i]
-            if not ( pl:IsPlayer() and ent:IsLineOfSightClear( pl:GetPos() ) ) then continue end
-            if ( pl:IsCombine() ) then continue end
-
-            -- do anything here...
-
-            --[[ Example
-
-            if _blacklist_weapon[pl:GetActiveWeapon():GetClass()] and (pl.b_CameraScanDelay or 0) < CurTime() then
-                self:EmitQueuedSound(ent, {"npc/scanner/scanner_siren1.wav", "npc/scanner/scanner_photo1.wav"})
-                ent:Fire("SetAngry")
-
-                ent:_SetSimpleTimer(5, function()
-                    ent:Fire("SetIdle")
-                end)
-
-                pl.b_CameraScanDelay = CurTime() + 5
-            end
-
-            --]]
+    for _, camera in ipairs(cameras_ents) do
+        local near_ents = ents.FindInSphere( camera:GetPos(), 335 )
+        for _, target in ipairs(near_ents) do
+            if not ( target:IsPlayer() and camera:IsLineOfSightClear( target:GetPos() ) ) then continue end
+            --...
         end
     end
 end
 
--- No breakable camera
 function PLUGIN:EntityTakeDamage(target, dmginfo)
-    if target:GetClass() == classname then
+    if target:GetClass() == "npc_combine_camera" then
         dmginfo:SetDamage(0)
     end
 end
