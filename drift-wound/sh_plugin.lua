daf282449dd4c67bdc59a4f0dd5ee34b258807d4
Bilwin <bilwin@minerva.pw>
2021-03-04 10:49:12 +0900

    Update sh_plugin.lua

--- drift-wound/sh_plugin.lua
+++ drift-wound/sh_plugin.lua
@@ -1,9 +1,9 @@
-local plugin = PLUGIN
-plugin.name = "Wounds"
-plugin.author = "Bilwin"
-plugin.description = "..."
-plugin.schema = "any"
-plugin.license = [[
+
+PLUGIN.name = "Wounds"
+PLUGIN.author = "Bilwin"
+PLUGIN.description = "..."
+PLUGIN.schema = "any"
+PLUGIN.license = [[
     Copyright 2021 Maxim Sukharev (Bilwin) All Rights Reserved
     This plugin is protected under by MPL-2.0 license
     Full copy of license is here: https://www.mozilla.org/en-US/MPL/2.0/
@@ -12,13 +12,15 @@ plugin.license = [[
 ix.char.RegisterVar("bIsBleeding", { 
     field = "bIsBleeding",
     fieldType = ix.type.bool,
-    default = false
+    default = false,
+    bNoDisplay = true
 })
 
 ix.char.RegisterVar("bIsFractured", { 
     field = "bIsFractured",
     fieldType = ix.type.bool,
-    default = false
+    default = false,
+    bNoDisplay = true
 })
 
 local ix = ix or {}
