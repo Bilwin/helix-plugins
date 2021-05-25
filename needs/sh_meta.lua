5d5e4582cb58030a0f9daae0b33a7413b2d1561b
Maxim <marf.zpd@gmail.com>
2021-05-25 19:55:02 +0900

    Update sh_meta.lua

--- needs/sh_meta.lua
+++ needs/sh_meta.lua
@@ -3,50 +3,34 @@ local playerMeta = FindMetaTable("Player")
 
 function playerMeta:GetSaturation()
     local char = self:GetCharacter()
-    return self:GetLocalVar("saturation"), char and char:GetSaturation()
+    return self:GetLocalVar("saturation"), (char and char:GetSaturation())
 end
 
 function playerMeta:GetSatiety()
     local char = self:GetCharacter()
-    return self:GetLocalVar("satiety"), char and char:GetSatiety()
+    return self:GetLocalVar("satiety"), (char and char:GetSatiety())
 end
 
 if (SERVER) then
     local CHAR = ix.meta.character
 
     function CHAR:DowngradeSatiety(value)
-        local client = self:GetPlayer()
-        if (self and IsValid(client)) then
-            local clamped = math.Round(math.Clamp(self:GetSatiety() - value, 0, 100))
-            client:SetLocalVar("satiety", clamped)
-            self:SetSatiety(clamped)
-        end
+        local calculated = math.Round(math.Clamp(self:GetSatiety() - value, 0, 100))
+        self:SetSatiety(calculated)
     end
 
     function CHAR:DowngradeSaturation(value)
-        local client = self:GetPlayer()
-        if (self and IsValid(client)) then
-            local clamped = math.Round(math.Clamp(self:GetSaturation() - value, 0, 100))
-            client:SetLocalVar("saturation", clamped)
-            self:SetSaturation(clamped)
-        end
+        local calculated = math.Round(math.Clamp(self:GetSaturation() - value, 0, 100))
+        self:SetSaturation(calculated)
     end
 
     function CHAR:RestoreSatiety(value)
-        local client = self:GetPlayer()
-        if (self and IsValid(client)) then
-            local clamped = math.Round(math.Clamp(self:GetSatiety() + value, 0, 100))
-            client:SetLocalVar("satiety", clamped)
-            self:SetSatiety(clamped)
-        end
+        local calculated = math.Round(math.Clamp(self:GetSatiety() + value, 0, 100))
+        self:SetSatiety(calculated)
     end
 
     function CHAR:RestoreSaturation(value)
-        local client = self:GetPlayer()
-        if (self and IsValid(client)) then
-            local clamped = math.Round(math.Clamp(self:GetSaturation() + value, 0, 100))
-            client:SetLocalVar("saturation", clamped)
-            self:SetSaturation(clamped)
-        end
+        local calculated = math.Round(math.Clamp(self:GetSaturation() + value, 0, 100))
+        self:SetSaturation(calculated)
     end
 end
