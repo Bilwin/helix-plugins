06f20094694f009c3c29a61493d3034952376b91
Maxim <marf.zpd@gmail.com>
2021-05-25 11:07:19 +0900

    Sanity update

--- needs/sh_meta.lua
+++ needs/sh_meta.lua
@@ -15,36 +15,36 @@ if (SERVER) then
     local CHAR = ix.meta.character
 
     function CHAR:DowngradeSatiety(value)
-        if (self and IsValid(self:GetPlayer())) then
+        local client = self:GetPlayer()
+        if (self and IsValid(client)) then
             local clamped = math.Round(math.Clamp(self:GetSatiety() - value, 0, 100))
-            local client = self:GetPlayer()
             client:SetLocalVar("satiety", clamped)
             self:SetSatiety(clamped)
         end
     end
 
     function CHAR:DowngradeSaturation(value)
-        if (self and IsValid(self:GetPlayer())) then
+        local client = self:GetPlayer()
+        if (self and IsValid(client)) then
             local clamped = math.Round(math.Clamp(self:GetSaturation() - value, 0, 100))
-            local client = self:GetPlayer()
             client:SetLocalVar("saturation", clamped)
             self:SetSaturation(clamped)
         end
     end
 
     function CHAR:RestoreSatiety(value)
-        if (self and IsValid(self:GetPlayer())) then
+        local client = self:GetPlayer()
+        if (self and IsValid(client)) then
             local clamped = math.Round(math.Clamp(self:GetSatiety() + value, 0, 100))
-            local client = self:GetPlayer()
             client:SetLocalVar("satiety", clamped)
             self:SetSatiety(clamped)
         end
     end
 
     function CHAR:RestoreSaturation(value)
-        if (self and IsValid(self:GetPlayer())) then
+        local client = self:GetPlayer()
+        if (self and IsValid(client)) then
             local clamped = math.Round(math.Clamp(self:GetSaturation() + value, 0, 100))
-            local client = self:GetPlayer()
             client:SetLocalVar("saturation", clamped)
             self:SetSaturation(clamped)
         end
