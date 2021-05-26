7ee9f75cb50fb0874ffcb60f7b08c581a206c6e9
Bilwin <marf.zpd@gmail.com>
2021-05-26 15:08:48 +0900

    Updated fog code

--- fog.lua
+++ fog.lua
@@ -1,19 +1,21 @@
-PLUGIN.name = "Fog"
-PLUGIN.author = "Bilwin"
-PLUGIN.description = "..."
-PLUGIN.schema = "Any"
 
-local ix = ix or {}
-ix.Seasons = ix.Seasons or {
+local PLUGIN = PLUGIN
+PLUGIN.name = "Seasons"
+PLUGIN.author = "Bilwin"
+PLUGIN.description = "Adjusts fog options"
+PLUGIN.schema = "Any"
+PLUGIN.version = 1.01
+PLUGIN.Active = "winter"
+PLUGIN.Seasons = {
     ["winter"] = {
-        fogColor = Vector( 135, 206, 235 ),
+        fogColor = Vector(135, 206, 235),
         fogStart = 500,
         fogEnd = 1300,
         fogDensity = 0.15,
         fogFade = 0
     },
     ["summer"] = {
-        fogColor = Vector( 170, 100, 31 ),
+        fogColor = Vector(170, 100, 31),
         fogStart = 500,
         fogEnd = 1300,
         fogDensity = 0.15,
@@ -21,28 +23,26 @@ ix.Seasons = ix.Seasons or {
     }
 }
 
-ix.Seasons.Active = "winter"
-
-if ( CLIENT ) then
-    local _data = ix.Seasons[ix.Seasons.Active]
-    local fog_data = {
-        ["fog_start"] = _data.fogStart,
-        ["fog_end"] = _data.fogEnd,
-        ["fog_color"] = _data.fogColor,
-        ["fog_max_density"] = _data.fogDensity,
-        ["fog_fade"] = _data.fogFade
+if (CLIENT) then
+    local activeSeasonData = PLUGIN.Seasons[PLUGIN.Active]
+    local newFogData = {
+        ["fog_start"] = activeSeasonData.fogStart,
+        ["fog_end"] = activeSeasonData.fogEnd,
+        ["fog_color"] = activeSeasonData.fogColor,
+        ["fog_max_density"] = activeSeasonData.fogDensity,
+        ["fog_fade"] = activeSeasonData.fogFade
     }
 
     local function SetupFog()
         render.FogMode(MATERIAL_FOG_LINEAR)
-            render.FogStart(fog_data.fog_start)
-            render.FogEnd(fog_data.fog_end)
-            render.FogColor(fog_data.fog_color.r, fog_data.fog_color.g, fog_data.fog_color.b)
-        render.FogMaxDensity(math.Clamp(fog_data.fog_max_density * ( fog_data.fog_fade or 0 ), 0.19, 1))
+            render.FogStart(newFogData.fog_start)
+            render.FogEnd(newFogData.fog_end)
+            render.FogColor(newFogData.fog_color.r, newFogData.fog_color.g, newFogData.fog_color.b)
+        render.FogMaxDensity(math.Clamp(newFogData.fog_max_density * ( newFogData.fog_fade or 0 ), 0.19, 1))
 
         return true
     end
 
-    hook.Add("SetupWorldFog", "Seasons:SetupWorldFog", SetupFog)
-    hook.Add("SetupSkyboxFog", "Seasons:SetupWorldFogSnow", SetupFog)
+    PLUGIN.SetupWorldFog = SetupFog
+    PLUGIN.SetupSkyboxFog = SetupFog
 end
