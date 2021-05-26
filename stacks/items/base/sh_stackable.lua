3bf7706eea4da4cb6215c3ded359d36684da5902
Bilwin <marf.zpd@gmail.com>
2021-05-26 14:57:39 +0900

    Added stackable items plugin

---
+++ stacks/items/base/sh_stackable.lua
@@ -0,0 +1,62 @@
+
+ITEM.name = "Stackable Items Base"
+ITEM.description = "Stackable Item"
+ITEM.category = "Stackable"
+ITEM.model = Model('models/props_c17/TrapPropeller_Lever.mdl')
+ITEM.maxStacks = 16
+
+if (CLIENT) then
+	function ITEM:PaintOver(item, w, h)
+		draw.SimpleText(
+			item:GetData("stacks", 1), "DermaDefault", w - 5, h - 5,
+			color_white, TEXT_ALIGN_RIGHT, TEXT_ALIGN_BOTTOM, 1, color_black
+		)
+	end
+end
+
+ITEM.functions.combine = {
+	OnRun = function(firstItem, data)
+        local firstItemStacks = firstItem:GetData('stacks', 1)
+        local secondItem = ix.item.instances[data[1]]
+        local secondItemStacks = secondItem:GetData('stacks', 1)
+		local totalStacks = secondItemStacks + firstItemStacks
+
+        if (firstItem.uniqueID ~= secondItem.uniqueID) then return false end
+        if (totalStacks > firstItem.maxStacks) then return false end
+
+		firstItem:SetData('stacks', totalStacks)
+		secondItem:Remove()
+
+		return false
+	end,
+	OnCanRun = function(firstItem, data)
+		return true
+	end
+}
+
+ITEM.functions.split = {
+	name = "Split",
+	icon = "icon16/arrow_divide.png",
+	OnRun = function(item)
+		local client = item.player
+		local character = client:GetCharacter()
+		local itemUniqueID = item.uniqueID
+		local stacks = item:GetData('stacks', 1)
+		client:RequestString('Split', 'Please enter how many items you want to split', function(splitStack)
+			if !isnumber(tonumber(splitStack)) then client:Notify('Please enter a `number`') return false end
+			local cleanSplitStack = math.Round(math.abs(tonumber(splitStack)))
+			if (cleanSplitStack >= stacks) then return false end
+			local stackedCount = (stacks - cleanSplitStack)
+
+			if !character:GetInventory():Add(itemUniqueID, 1, {stacks = cleanSplitStack}) then
+				ix.item.Spawn(itemUniqueID, client, nil, angle_zero, {stacks = cleanSplitStack})
+			end
+
+			item:SetData('stacks', stackedCount)
+		end, '1')
+		return false
+	end,
+	OnCanRun = function(item)
+		return item:GetData('stacks', 1) ~= 1
+	end
+}
---
+++ stacks/sh_meta.lua
@@ -0,0 +1,36 @@
+
+local INVENTORY = ix.meta.inventory
+
+function INVENTORY:GetStackCount(uniqueID)
+	local originalList = ix.item.list[uniqueID]
+	local bIsStacking = originalList.base == 'base_stackable'
+	if (originalList and bIsStacking) then
+		local stackedCount = 0
+		for _, v in ipairs(self:GetItemsByUniqueID(uniqueID)) do
+			local realCount = v['data'].stacks or 1
+			stackedCount = stackedCount + realCount
+		end
+		return tonumber(stackedCount)
+	else
+		return 0
+	end
+end
+
+-- Replace original helix INVENTORY:GetItemCount function
+function INVENTORY:GetItemCount(uniqueID, onlyMain)
+	local originalList = ix.item.list[uniqueID]
+	local bIsStacking = originalList.base == 'base_stackable'
+	local i = 0
+
+	if (bIsStacking) then
+		i = self:GetStackCount(uniqueID)
+	else
+		for _, v in pairs( self:GetItems(onlyMain) ) do
+			if (v.uniqueID == uniqueID) then
+				i = i + 1
+			end
+		end
+	end
+
+	return i
+end
