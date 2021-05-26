566e434d2938cf599edc941f261f47cef0ab03cb
Bilwin <marf.zpd@gmail.com>
2021-05-26 15:31:12 +0900

    Create info_nodes.lua

---
+++ info_nodes.lua
@@ -0,0 +1,29 @@
+
+PLUGIN.name = "Nodes"
+PLUGIN.description = "Comment areas"
+PLUGIN.author = "Bilwin"
+PLUGIN.schema = "Any"
+
+function PLUGIN:SetupAreaProperties()
+    ix.area.AddType("comment")
+    ix.area.AddProperty("comment", ix.type.string, '')
+end
+
+if (CLIENT) then
+    function PLUGIN:OnAreaChanged(oldID, newID)
+        if LocalPlayer():GetMoveType() == MOVETYPE_NOCLIP then return; end
+        local storedData = ix.area.stored[newID]
+        for id, data in pairs( ix.area.stored ) do
+            if (id == newID and data.type == 'comment') then
+                hook.Run('EnteredCommentArea', data)
+            end
+        end
+    end
+
+    function PLUGIN:EnteredCommentArea(data)
+        local text = data['properties'].comment
+        if (text and text ~= '') then
+            chat.AddText(Color(255, 150, 0), text)
+        end
+    end
+end
