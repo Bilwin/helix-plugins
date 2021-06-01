28d54a41a2e346e755158d323a8e28c24a8bee19
Bilwin <marf.zpd@gmail.com>
2021-06-01 18:01:25 +0900

    `:GetItemCount` better performance (#3)
    Co-Authored-By: Sam Nyashev <12237569+Heyter@users.noreply.github.com>

--- stacks/sh_meta.lua
+++ stacks/sh_meta.lua
@@ -1,32 +1,17 @@
 
 local INVENTORY = ix.meta.inventory
 
-function INVENTORY:GetStackCount(uniqueID)
-	local originalList = ix.item.list[uniqueID]
-	local bIsStacking = originalList.base == 'base_stackable'
-	if (originalList and bIsStacking) then
-		local stackedCount = 0
-		for _, v in ipairs(self:GetItemsByUniqueID(uniqueID)) do
-			local realCount = v['data'].stacks or 1
-			stackedCount = stackedCount + realCount
-		end
-		return tonumber(stackedCount)
-	else
-		return 0
-	end
-end
-
--- Rewrite original helix INVENTORY:GetItemCount function
 function INVENTORY:GetItemCount(uniqueID, onlyMain)
-	local originalList = ix.item.list[uniqueID]
-	local bIsStacking = originalList.base == 'base_stackable'
 	local i = 0
+	local stacks
 
-	if (bIsStacking) then
-		i = self:GetStackCount(uniqueID)
-	else
-		for _, v in pairs( self:GetItems(onlyMain) ) do
-			if (v.uniqueID == uniqueID) then
+	for _, v in pairs(self:GetItems(onlyMain)) do
+		if (v.uniqueID == uniqueID) then
+			stacks = v.data.stacks
+
+			if (stacks and stacks >= 2) then
+				i = i + stacks
+			else
 				i = i + 1
 			end
 		end
