a77f80559cb986cc6d55d6e9b97470ba4ee7c047
Bilwin <marf.zpd@gmail.com>
2021-07-18 22:52:52 +0900

    Ambients

---
+++ ambients.lua
@@ -0,0 +1,126 @@
+
+local PLUGIN = PLUGIN
+local _tonumber = tonumber
+local _math_ceil = math.ceil
+local _SoundDuration = SoundDuration
+local _Ambients_Cooldown = 10
+
+PLUGIN.name = "Ambient Music"
+PLUGIN.description = "Adds background music (client-side)"
+PLUGIN.author = "Bilwin"
+PLUGIN.schema = "Any"
+PLUGIN.songs = {
+    {path = "music/hl2_song13.mp3", duration = 54},
+    {path = "music/hl2_song17.mp3", duration = 61},
+    {path = "music/hl2_song2.mp3", duration = 173},
+    {path = "music/hl2_song30.mp3", duration = 104},
+    {path = "music/hl2_song8.mp3", duration = 60},
+}
+
+ix.lang.AddTable("english", {
+	optEnableAmbient = "Enable ambient",
+    optAmbientVolume = "Ambient volume"
+})
+
+ix.lang.AddTable("russian", {
+	optEnableAmbient = "Включить фоновую музыку",
+    optAmbientVolume = "Громкость фоновой музыки"
+})
+
+if (CLIENT) then
+    m_flAmbientCooldown = m_flAmbientCooldown or 0
+    bAmbientPreSaver = bAmbientPreSaver or false
+
+    ix.option.Add("enableAmbient", ix.type.bool, true, {
+		category = PLUGIN.name,
+        OnChanged = function(oldValue, value)
+            if (value) then
+                if IsValid(PLUGIN.ambient) then
+                    local volume = ix.option.Get("ambientVolume", 1)
+                    PLUGIN.ambient:SetVolume(volume)
+                end
+            else
+                if IsValid(PLUGIN.ambient) then
+                    PLUGIN.ambient:SetVolume(0)
+                end
+            end
+        end
+	})
+
+	ix.option.Add("ambientVolume", ix.type.number, 0.5, {
+		category = PLUGIN.name,
+        min = 0.1,
+        max = 2,
+        decimals = 1,
+        OnChanged = function(oldValue, value)
+            if IsValid(PLUGIN.ambient) and ix.option.Get("enableAmbient", true) then
+                PLUGIN.ambient:SetVolume(value)
+            end
+        end
+	})
+
+    do
+        if !table.IsEmpty(PLUGIN.songs) then
+            for _, data in ipairs(PLUGIN.songs) do
+                util.PrecacheSound(data.path)
+            end
+        end
+    end
+
+    function PLUGIN:CreateAmbient()
+        local bEnabled = ix.option.Get('enableAmbient', true)
+
+        if (bEnabled and !bAmbientPreSaver) then
+            local flVolume = _tonumber(ix.option.Get('ambientVolume', 1))
+            local mSongTable = self.songs[math.random(1, #self.songs)]
+            local mSongPath = mSongTable.path
+            local mSongDuration = mSongTable.duration or _SoundDuration(mSongPath)
+
+            sound.play_file('sound/' .. mSongTable.path, 'noblock', function(radio)
+                if IsValid(radio) then
+                    if IsValid(self.ambient) then self.ambient:Stop() end
+
+                    radio:SetVolume(flVolume)
+                    radio:Play()
+                    self.ambient = radio
+
+                    m_flAmbientCooldown = os.time() + _tonumber(mSongDuration) + _Ambients_Cooldown
+                end
+            end)
+        end
+    end
+
+    net.Receive('ixPlayAmbient', function()
+        if !timer.exists('mAmbientMusicChecker') then
+            timer.create('mAmbientMusicChecker', 5, 0, function()
+                if (m_flAmbientCooldown or 0) > os.time() then return end
+                PLUGIN:CreateAmbient()
+            end)
+        end
+
+        if !timer.exists('mAmbientChecker') then
+            timer.create('mAmbientChecker', 0.5, 0, function()
+                if IsValid(ix.gui.characterMenu) and ix.config.Get("music") ~= "" then
+                    if IsValid(PLUGIN.ambient) then
+                        PLUGIN.ambient:SetVolume(0)
+                    end
+                else
+                    if ix.option.Get('enableAmbient', true) then
+                        if IsValid(PLUGIN.ambient) then
+                            local volume = ix.option.Get("ambientVolume", 1)
+                            PLUGIN.ambient:SetVolume(volume)
+                        end
+                    end
+                end
+            end)
+        end
+    end)
+end
+
+if (SERVER) then
+    util.AddNetworkString('ixPlayAmbient')
+    function PLUGIN:PlayerLoadedCharacter(client, character, currentChar)
+        net.Start('ixPlayAmbient')
+        net.Send(client)
+    end
+end
