ca980abc01d5443a0c7e4eeba0979d054eabe1b3
Bilwin <bilwin780@gmail.com>
2021-10-02 17:23:23 +0900

    Update sv_hooks.lua

--- radios/sv_hooks.lua
+++ radios/sv_hooks.lua
@@ -7,7 +7,7 @@ net.Receive("ixRadio", function(_, pl)
     if !IsValid(radio) and !IsEntity(radio) then return end
     if radio:GetClass() ~= "ix_radio" then return end
 
-	if radio:GetPos():DistToSqr( pl:GetPos() ) > 160 then return end
+	if radio:GetPos():DistToSqr( pl:GetPos() ) > 50000 then return end
 	radio:SelectSong( songPath )
 end)
 
