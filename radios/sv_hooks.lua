6daac64ca99a0891f4902f843849aa32863fa7c0
Maksim <marf.zpd@gmail.com>
2021-10-04 10:02:42 +0900

    Update sv_hooks.lua

--- radios/sv_hooks.lua
+++ radios/sv_hooks.lua
@@ -2,10 +2,10 @@
 util.AddNetworkString('ixRadio')
 
 net.Receive("ixRadio", function(_, pl)
-    local radio = net.ReadEntity()
-    local songPath = net.ReadString()
-    if !IsValid(radio) and !IsEntity(radio) then return end
-    if radio:GetClass() ~= "ix_radio" then return end
+    	local radio = net.ReadEntity()
+    	local songPath = net.ReadString()
+    	if !IsValid(radio) and !IsEntity(radio) then return end
+    	if radio:GetClass() ~= "ix_radio" then return end
 
 	if radio:GetPos():DistToSqr( pl:GetPos() ) > 50000 then return end
 	radio:SelectSong( songPath )
