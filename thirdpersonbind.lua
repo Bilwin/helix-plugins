0c9bd0e9e51d97e74ff1f2750b26dd3e6bffd80b
Bilwin <bilwin780@gmail.com>
2021-10-19 12:27:02 +0900

    Update thirdpersonbind.lua

--- thirdpersonbind.lua
+++ thirdpersonbind.lua
@@ -7,8 +7,8 @@ PLUGIN.bind = KEY_P
 if (CLIENT) then
     local Cooldown
     function PLUGIN:PlayerButtonDown(me, button)
-        if (button == self.bind) then
-            if (IsFirstTimePredicted()) then
+        if button == self.bind then
+            if IsFirstTimePredicted() then
                 if (Cooldown or 0) < CurTime() then
                     RunConsoleCommand("ix_togglethirdperson")
                     Cooldown = CurTime() + 0.1
