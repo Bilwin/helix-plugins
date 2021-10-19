d45d90573f924ae40ec2c264b29405d8ae48429b
Bilwin <bilwin780@gmail.com>
2021-10-19 12:27:53 +0900

    Update garbagecleaner.lua

--- garbagecleaner.lua
+++ garbagecleaner.lua
@@ -6,32 +6,30 @@ PLUGIN.description = "Just garbage (ix_item) cleaner"
 PLUGIN.schema = "Any"
 PLUGIN.garbage = {'ix_item'}
 
-do
-    ix.config.Add("garbageCleanerEnabled", true, "Enable garbage (ix_item) cleaner?", function(_, newValue)
-        if (SERVER) then
-            if (timer.Exists('GarbageCleaner')) then
-                if (newValue) then
-                    timer.Start('GarbageCleaner')
-                else
-                    timer.Stop('GarbageCleaner')
-                end
+ix.config.Add("garbageCleanerEnabled", true, "Enable garbage (ix_item) cleaner?", function(_, newValue)
+    if SERVER then
+        if timer.Exists('GarbageCleaner') then
+            if newValue then
+                timer.Start('GarbageCleaner')
+            else
+                timer.Stop('GarbageCleaner')
             end
         end
-    end, {
-        category = PLUGIN.name
-    })
+    end
+end, {
+    category = PLUGIN.name
+})
 
-    ix.config.Add("garbageCleanerDelay", 300, "How many seconds does a timer tick?", function(_, newValue)
-        if (SERVER) then
-            if (timer.Exists('GarbageCleaner')) then
-                timer.Adjust('GarbageCleaner', newValue)
-            end
+ix.config.Add("garbageCleanerDelay", 300, "How many seconds does a timer tick?", function(_, newValue)
+    if SERVER then
+        if timer.Exists('GarbageCleaner') then
+            timer.Adjust('GarbageCleaner', newValue)
         end
-    end, {
-        data = {min = 1, max = 1200},
-        category = PLUGIN.name
-    })
-end
+    end
+end, {
+    data = {min = 1, max = 1200},
+    category = PLUGIN.name
+})
 
 if (SERVER) then
     function PLUGIN:CreateGarbageCleaner(delay)
