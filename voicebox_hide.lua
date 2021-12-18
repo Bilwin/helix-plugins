055602bbc3c3ad56e1d8c46eec2c29f4450bb5c4
Bilwin <bilwin780@gmail.com>
2021-12-18 17:59:31 +0900

    Create voicebox_hide.lua

---
+++ voicebox_hide.lua
@@ -0,0 +1,12 @@
+
+local PLUGIN = PLUGIN
+PLUGIN.name = 'VoiceBox Remover'
+PLUGIN.author = 'Bilwin'
+
+if CLIENT then
+    function PLUGIN:PlayerStartVoice()
+        if IsValid(g_VoicePanelList) then
+            g_VoicePanelList:Remove()
+        end
+    end
+end
