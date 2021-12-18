2a295c1529a6348e3ba348cbb5bae478e609dfe9
Bilwin <bilwin780@gmail.com>
2021-12-18 17:35:21 +0900

    Update sh_plugin.lua

--- languages/sh_plugin.lua
+++ languages/sh_plugin.lua
@@ -1,7 +1,7 @@
 
 PLUGIN.name = "Languages"
 PLUGIN.author = "Bilwin"
-PLUGIN.version = 1.0
+PLUGIN.version = 1.01
 PLUGIN.schema = "Any"
 PLUGIN.license = [[
     This is free and unencumbered software released into the public domain.
@@ -26,6 +26,9 @@ PLUGIN.license = [[
     For more information, please refer to <http://unlicense.org/>
 ]]
 
+ix.util.Include("sh_languages.lua")
+ix.util.Include("sh_meta.lua")
+
 ix.char.RegisterVar("additionalLanguages", {
 	field = "additionalLanguages",
 	fieldType = ix.type.string,
@@ -33,41 +36,40 @@ ix.char.RegisterVar("additionalLanguages", {
 	bNoDisplay = true
 })
 
-ix.command.Add("CharAddLanguage", {
-	description = "Let the specified player speak the specified language",
-	adminOnly = true,
-	arguments = {ix.type.character, ix.type.string},
-	OnRun = function(self, client, character, language)
-		if character and IsValid(character:GetPlayer()) then
-			if ix.languages:GetAll()[language] then
-				character:AddLanguage(language)
-				client:Notify('Character '..character:GetName()..' ('..character:GetPlayer():SteamID()..') successfully issued knowledge of the language: '..ix.languages:GetAll()[language].name)
+function PLUGIN:InitializedChatClasses()
+	ix.command.Add("CharAddLanguage", {
+		description = "Let the specified player speak the specified language",
+		adminOnly = true,
+		arguments = {ix.type.character, ix.type.string},
+		OnRun = function(self, client, character, language)
+			if character and IsValid(character:GetPlayer()) then
+				if ix.languages:GetAll()[language] then
+					character:AddLanguage(language)
+					client:Notify('Character '..character:GetName()..' ('..character:GetPlayer():SteamID()..') successfully issued knowledge of the language: '..ix.languages:GetAll()[language].name)
+				else
+					client:Notify('The specified language was not found.')
+				end
 			else
-				client:Notify('The specified language was not found.')
+				client:Notify('There is no such player')
 			end
-		else
-			client:Notify('There is no such player')
 		end
-	end
-})
+	})
 
-ix.command.Add("CharRemoveLanguage", {
-	description = "Take away from the specified character the ability to speak the specified language",
-	adminOnly = true,
-	arguments = {ix.type.character, ix.type.string},
-	OnRun = function(self, client, character, language)
-		if character and IsValid(character:GetPlayer()) then
-			if ix.languages:GetAll()[language] then
-				character:RemoveLanguage(language)
-				client:Notify('You have successfully removed the ability to speak in '..ix.languages:GetAll()[language].name..' language to the character '..character:GetName()..' ('..character:GetPlayer():SteamID()..')')
+	ix.command.Add("CharRemoveLanguage", {
+		description = "Take away from the specified character the ability to speak the specified language",
+		adminOnly = true,
+		arguments = {ix.type.character, ix.type.string},
+		OnRun = function(self, client, character, language)
+			if character and IsValid(character:GetPlayer()) then
+				if ix.languages:GetAll()[language] then
+					character:RemoveLanguage(language)
+					client:Notify('You have successfully removed the ability to speak in '..ix.languages:GetAll()[language].name..' language to the character '..character:GetName()..' ('..character:GetPlayer():SteamID()..')')
+				else
+					client:Notify('The specified language was not found.')
+				end
 			else
-				client:Notify('The specified language was not found.')
+				client:Notify('There is no such player')
 			end
-		else
-			client:Notify('There is no such player')
 		end
-	end
-})
-
-ix.util.Include("sh_languages.lua")
-ix.util.Include("sh_meta.lua")
+	})
+end
