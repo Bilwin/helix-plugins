534dfac020bd30064846a10ce5c6a2fcb0e8fd0c
Maxim <bilwin780@gmail.com>
2021-12-19 11:25:52 +0900

    logic fix

--- fixstep.lua
+++ fixstep.lua
@@ -7,7 +7,7 @@ if CLIENT then
     local steps = {'.stepleft', '.stepright'}
     function PLUGIN:EntityEmitSound(data)
         if ix.option.Get('thirdpersonEnabled', false) then
-            if !IsValid(data.Entity) || !data.Entity:IsPlayer() then return end
+            if !IsValid(data.Entity) && !data.Entity:IsPlayer() then return end
             local sName = data.OriginalSoundName
             if sName:find(steps[1]) || sName:find(steps[2]) then
                 return false
