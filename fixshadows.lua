c8211e92d20cb5677ab374a0d84aa414fc50edb1
Bilwin <me@bilw.in>
2023-01-29 23:26:41 +0900

    :lipstick: refactor

--- fixshadows.lua
+++ fixshadows.lua
@@ -3,7 +3,7 @@ PLUGIN.name = 'Shadows Overhaul'
 if SERVER then
     function PLUGIN:InitPostEntity()
         for _, v in ipairs( ents.FindByClass('prop_door_rotating') ) do
-            if IsValid(v) and v:IsDoor() then
+            if IsValid(v) && v:IsDoor() then
                 v:DrawShadow(false) -- we need this?
             end
         end
@@ -12,14 +12,15 @@ end
 
 if CLIENT then
     timer.Create('FixShadows', 10, 0, function() -- same
-        for _, player in ipairs( player.GetAll() ) do
-            player:DrawShadow(false)
-        end
-
-        for _, v in ipairs( ents.FindByClass('prop_door_rotating') ) do
-            if IsValid(v) && v:IsDoor() then
-                v:DrawShadow(false)
-            end
+        for _, client in ipairs(player.GetAll()) do
+            client:DrawShadow(false)
         end
     end)
+
+    function PLUGIN:InitPostEntity()
+        for _, v in ipairs( ents.FindByClass('prop_door_rotating') ) do
+            if not IsValid(v) then continue end
+            v:DrawShadow(false)
+        end
+    end
 end
