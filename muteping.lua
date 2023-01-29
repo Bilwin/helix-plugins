734a5506dbbdd955e4f7806d7a2fb0d542241c59
Bilwin <me@bilw.in>
2023-01-29 23:27:23 +0900

    Update muteping.lua

--- muteping.lua
+++ muteping.lua
@@ -3,7 +3,7 @@ PLUGIN.description  = 'Mute Combine Camera "beep" pings'
 PLUGIN.author       = 'Bilwin'
 
 function PLUGIN:EntityEmitSound(soundData)
-    if soundData.Entity and IsValid(soundData.Entity) then
+    if soundData.Entity && IsValid(soundData.Entity) then
         if soundData.Entity:GetClass() == 'npc_combine_camera' then
             local soundName = soundData.SoundName
             if soundName == 'npc/turret_floor/ping.wav' then
