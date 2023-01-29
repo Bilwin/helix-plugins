cdcf36651e9194b3ee44185da554df2471d922f1
Bilwin <me@bilw.in>
2023-01-29 23:27:33 +0900

    Update muteping.lua

--- muteping.lua
+++ muteping.lua
@@ -5,8 +5,7 @@ PLUGIN.author       = 'Bilwin'
 function PLUGIN:EntityEmitSound(soundData)
     if soundData.Entity && IsValid(soundData.Entity) then
         if soundData.Entity:GetClass() == 'npc_combine_camera' then
-            local soundName = soundData.SoundName
-            if soundName == 'npc/turret_floor/ping.wav' then
+            if soundData.SoundName == 'npc/turret_floor/ping.wav' then
                 return false
             end
         end
